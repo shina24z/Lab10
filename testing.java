28215b850815067de36b1c565743128034819f24
shina24z <142839441+shina24z@users.noreply.github.com>
2023-12-23 17:14:37 +0000

    Imported a Scanner

--- testing.java
+++ testing.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class testing{
     public static void main(String[]args){
+
+        Scanner scan = new Scanner(System.in);
         System.out.println("hello world");
         
     }
